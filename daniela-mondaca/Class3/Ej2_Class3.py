cc35e7819d2d5ec5a6434b2bf45955895b903d11
deria! <118624580+deriaaa@users.noreply.github.com>
2022-11-23 11:53:20 -0400

    update ej2

--- daniela-mondaca/Class3/Ej2_Class3.py
+++ daniela-mondaca/Class3/Ej2_Class3.py
@@ -1,35 +1,22 @@
-
-
-
 def imp():
     for i in range(4):
         if (i==0): L=int(input("Ingresa L "'\n')); L=L-1
-        if (L>1): keep=False; 
-        if(L<2): print("El tamaño del mundo no puede ser uno o menor")
         elif (i==1): p=int(input("Ingresa p "+'\n')) 
         elif (i==2): q=int(input("Ingresa q "+'\n')) 
         elif (i==3): n=int(input("Ingresa n "+'\n'))
-    return(L, p, q, n, keep)
+    return(L, p, q, n)
 
-keep=True
-while(keep==True):  
-    L, p, q, n, keep=imp() 
-
-#wrldif
-def wdif(p1,L1):
-    
-
-    return(p1)
+ 
+L, p, q, n=imp() 
 
 
 #jmp
 for i in range(n):
     print("Juanito se encuentra en la casilla "+str(p))
     print("Avanzara "+str(q)+" casillas")
+    strt=p
     p=p+q
-    if(p>L):
-        dif=int(p-L)
-        p=p-dif
+    
     print("Juanito avanzo hasta: "+str(p))
 
 
