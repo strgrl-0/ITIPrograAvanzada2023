41a39c96f06a328f004393b60bfeb807cbfcf956
Deria <_deria@localhost.localdomain>
2022-12-01 22:10:32 -0300

    reqs 1 and 2 complete, added comments for code understanding, glossary underway

--- daniela-mondaca/simulacro prueba 2/code1.py
+++ daniela-mondaca/simulacro prueba 2/code1.py
@@ -2,7 +2,7 @@
 #arrays = 6,6. 100,100.
 #si una ubicación fuera a tener más de 100 items, se despachan inmediatamente 100 items, se conserva el excedente.
 
-
+################ initial data reading (textfile->list)##############
 
 #import numpy
 import numpy as np
@@ -20,7 +20,7 @@ def add(list, elm):
 #zone with merged sectlor to zone and sector
 def zAs(nStore):
     spls=nStore.split('-')
-    zone=spls[0]; sector=spls[1]
+    zone=spls[0]; sector=int(spls[1]); sector-=1
     return(zone, sector)
         
         
@@ -45,32 +45,63 @@ datext,zonext,sectext,amtext=ship(); inStore=np.zeros([6,6]); sentt=np.zeros([6,
 
 
 
-#sent packages to secondary array
-def snt(Wz,Ws,AWZ):
-    sentt[Wz][Ws]=sentt[Wz][Ws]+100
-    print("Se han enviado 100 paquetes a "+str(AWZ)+" - "+str(Ws))
+########################################### secondary reading (list->array) and embedded processing (requests 1 and 2)
 
-#translate letter zone to numerical index
+
+#sent packages to secondary array (part of req 2)
+def snt(Wz,Ws,AWZ,i):
+    sentt[Wz][Ws]=sentt[Wz][Ws]+1
+    print("Se han despachado 100 paquetes a "+str(AWZ[i])+" - "+str(Ws+1),'\n')
+
+#total sents calculation (part of req 2)
+def totalsents(totalsents):
+    dtotal=totalsents
+    totalN=int()
+    for z in range(6):
+        for s in range(6):
+            if dtotal[z][s]!=0:
+                dtotal[z][s]=dtotal[z][s]
+                totalN=totalN+dtotal[z][s]
+    return(totalN)
+
+
+
+
+#translate letter zone to numerical index (secondary processing list to array)
 def trZ(zonetr,i):
     ztN=["A","B","C","D","E","F"]
-    convt=0
-    for c in range(0,5):        
+    convt=int()
+    for c in range(0,6):        
         if ztN[c]==zonetr[i]:
             convt=c
     return(convt)
                 
-#translate to main storage array
+#translate to main storage array (secondary processing list to array - main code, embedded processing (req 1 -determines when and where a shipment takes place- and 
+# req 2 -determines how many shipments have been made and to where, shows said info via array))
 
 def trsmtx(zone,sector,amt,date):
-    for i in range(len(zone)):
-        print(i)
+    for i in range(len(date)):
         z=trZ(zone,i); s=int(sector[i])
         inStore[z][s]=inStore[z][s]+amt[i]
         if inStore[z][s]>=100:
-            snt(z,s,zone)
+            print("100 paquetes fueron despachados el "+str(date[i])+" hacia "+str(zone[i])+" - "+str(s+1))
+            snt(z,s,zone,i)
             inStore[z][s]=inStore[z][s]-100
-            print("100 paquetes fueron despachados el "+str(date)+" hacia "+str(zone)+" - "+str(s))
+    
+    #total sents (minimal processing and output of req 2)
+    totalN=int(totalsents(sentt)); totalN=totalN*100
+    print("Se envio un total de "+str(totalN)+" paquetes durante este mes",'\n')
+    print("Los paquetes fueron despachados a las direcciones que se muestran a continuación",'\n')
+    print(sentt)
 
 
+        
+'''debug line begin
+        print(i, z, str(sector[i])+'\n')
+        print(inStore, '\n')
+        #debug line ends'''
+
+
+ 
 trsmtx(zonext,sectext,amtext,datext)
 
